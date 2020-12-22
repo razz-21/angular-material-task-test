9a9e978993be4da45a7ab7f9d9468ee939f95c37
Ernesto Razo <ernesto@hyperstacksinc.com>
2020-12-22 10:46:10 +0800

    refactor: update components and add calendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,16 @@ import { AbstractControl, FormControl, Validators } from '@angular/forms';
 export class AppComponent {
 
   autoClearField: FormControl;
-  emailField: FormControl;
-  title = 'test-task';
+  errorField: FormControl;
 
   constructor() {
-    this.autoClearField = new FormControl('');
-    this.emailField = new FormControl(
-      '',
+    this.autoClearField = new FormControl('Text entered, the input field is completed.');
+    this.errorField = new FormControl(
+      'Text that user entering.',
       [Validators.required, Validators.email]
-    )
+    );
+    this.errorField.markAsTouched();
+    this.errorField.markAsDirty();
   }
 
   clearFormField(formControl: AbstractControl): void {
